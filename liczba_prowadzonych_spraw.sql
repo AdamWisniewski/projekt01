97e781307c0bc91c8ffb563c9ef5278f51d5d87f
adam_wisniewski <awisniewski27@gmail.com>
2017-11-29 18:11:53 +0100

    weryfikacja bazy danych

--- liczba_prowadzonych_spraw.sql
+++ liczba_prowadzonych_spraw.sql
@@ -2,6 +2,7 @@
     SELECT 
         pracownik_imie,
         pracownik_nazwisko,
+        stanowisko_pracownik,
         liczba_prowadzonych_spraw,
         waga_prowadzonych_spraw
     FROM
@@ -14,9 +15,11 @@
         FROM
             sprawy
         WHERE
-            decyzja_numer IS NULL AND sprawa_status != 5
+            decyzja_numer IS NULL AND sprawa_status != 5  #nie zliczamy spraw zakończonych(wydana decyzja) oraz spraw zawieszonych
         GROUP BY pracownik_prowadzacy_ID) AS t ON p.ID = t.pracownik_prowadzacy_ID
-        ORDER BY liczba_prowadzonych_spraw DESC;
+			NATURAL LEFT JOIN
+		stanowisko AS s 
+        ORDER BY stanowisko_pracownik, pracownik_nazwisko;
  
  #CREATE VIEW sprawy_w_toku AS
     SELECT 
@@ -34,7 +37,14 @@
 		sprawy,
         sprawa_status
 	order by data_wniosku;
-        
- select * from sprawa_status;
+ 
+ 
+ select * from sprawy;
+ 
+ 
+ load data local infile 'C:/Users/Kasia/Desktop/sprawy.txt' into table sprawy;
+ 
+ Select p.*, s.* from pracownicy p natural left join stanowisko s;
+ 
  
 # pomysł na trigger - jeżeli wpiszę numer decyzji to automatycznie status sprawy zmienia się na zakończona
