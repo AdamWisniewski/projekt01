45b85883ee1bc123276f8cd272e8a1b7e217c144
adam_wisniewski <awisniewski27@gmail.com>
2017-12-05 19:36:58 +0100

    gotowa klasa PublicUser

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def connectDatabase():
  
     
 # klasy przedstawiające rodzaje użytkowników serwisu
+# pomyśleć jak skrócić wiersz definiujący wiświetlanie tabeli
 
 class PublicUser:
     def __init__(self):
@@ -18,10 +19,28 @@ class PublicUser:
         print('Wyświetl wszystkie sprawy w trakcie [1]\nWyświetl wszystkie sprawy zakończone[2]\nSprawdź postępowania dla wskazanego adresu[3]\n')
         
     def dispAllProceduresFinished(self):
-        c.execute('select * from pracownicy')
+        c.execute('select * from sprawy where decyzja_numer is not null')
         for row in c:
-            print (row)        
+            print("|%3i|%3i|%10s|%12s|%15s|%12.12s|%20.20s|%3i|%-35s|%3s|%3s|%3s|" %(row[0], row[1], str(row[2]), row[3], row[4], row[5], row[6], row[7], row[8], str(row[13]), row[14], row[15]))
+            
+    def dispAllProceduresInProgress(self):
+        c.execute('select * from sprawy where decyzja_numer is null')
+        for row in c:
+            print("|%3i|%3i|%10s|%12s|%15s|%12.12s|%20.20s|%3i|%-35s|" %(row[0], row[1], str(row[2]), row[3], row[4], row[5], row[6], row[7], row[8]))    
+    
+    def dispAllProcedures(self):
+        c.execute('select * from sprawy')
+        for row in c:
+            print(wzorzec(self))   
         
+    def dispProceduresForAdress(self):
+        adres = str(input('podaj szukany adres: '))
+        adres = str('\'' + adres + '\'')
+        c.execute('select * from sprawy where sprawa_adres =' + adres) # rozbić ulicę i numerw bazie by ułatwić wyszukiwanie
+        for row in c:
+            print("|%3i|%3i|%10s|%12s|%15s|%12.12s|%20.20s|%3i|%-35s|%3s|%3s|%3s|" %(row[0], row[1], str(row[2]), row[3], row[4], row[5], row[6], row[7], row[8], str(row[13]), row[14], row[15]))
+
+
 connectDatabase()
 a = PublicUser()
-a.dispAllProceduresFinished()
+a.dispProceduresForAdress()5
